6df225e04da91ee2dd6fe9b26e8f3046d76347e2
yl698 <yl698@cornell.edu>
2021-04-11 03:49:28 -0400

    auto format

--- app.py
+++ app.py
@@ -3,11 +3,12 @@ from app.accounts.models.user import User
 import click
 
 @click.command()
-@click.option('--port', default=5000, prompt='port to use', help='The port to use for the app')
+@click.option('--port', default=5000, prompt='port to use',
+              help='The port to use for the app')
 def init(port):
-  print("Flask app running at http://0.0.0.0:{}".format(port))
-  socketio.run(app, host="0.0.0.0", port=port)
+    print("Flask app running at http://0.0.0.0:{}".format(port))
+    socketio.run(app, host="0.0.0.0", port=port)
+
 
 if __name__ == "__main__":
-  init()
-
+    init()
