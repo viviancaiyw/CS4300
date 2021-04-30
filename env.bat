44c16e8f2cb4ebd96b8344764013be222e98417e
Ethan Hu <ethan.hqc@icloud.com>
2021-04-30 07:37:48 -0400

    sync env.bat with .env for windows developers : )

--- env.bat
+++ env.bat
@@ -1,4 +1,4 @@
 :: Set the environment type of the app (see config.py)
 SET APP_SETTINGS=config.DevelopmentConfig
 :: Set the DB url to a local database for development
-SET DATABASE_URL=postgresql://localhost/my_app_db
+SET DATABASE_URL=postgres://bivxusanexnbjp:12f8e000800ba66e4f98d0df4795edc962bdfd5318cbd9f19c7eaf16ef244f54@ec2-52-23-45-36.compute-1.amazonaws.com:5432/dd46p10a83f3m0
