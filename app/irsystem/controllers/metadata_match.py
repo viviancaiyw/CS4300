fde501c6ec1f537c57f5ae39bfad23005929a8f3
Grace Zhao <rz92@cornell.edu>
2021-04-30 15:20:08 -0400

    update metadata_match

--- app/irsystem/controllers/metadata_match.py
+++ app/irsystem/controllers/metadata_match.py
@@ -9,18 +9,18 @@ def _gen_sql_query(raw_genre_list):
     raw_genre_list: list of raw user genre input from the UI
     Returns: The sqlalchemy query for a give genre list to filter the game by    
     """
+    genre_list = []
     if raw_genre_list is not None:
-        genre_list = []
         for genre in raw_genre_list:
-            genre_list.append(GENRE_KEY[genre])
-        print(genre_list)
-        query = """Game.query.filter(Game.single_player == singleplayer, Game.multi_player == multiplayer,("""
-        for genre in genre_list:
-            query = query + "Game.genre.like('%{}%')|".format(genre)
+            genre_list.append(GENRE_KEY.get(genre, ''))
+    query = "Game.query.filter(((Game.single_player == singleplayer) | (Game.multi_player == multiplayer)), ("
+    for genre in genre_list:
+        query = query + "Game.genre.like('%{}%')|".format(genre)
+    if len(genre_list):
         query = query[:-1] + ')).all()'
-        return query
     else:
-        return None
+        query = query[:-2] + ').all()'
+    return query
 
 
 def filter_games(singleplayer: bool, multiplayer: bool, raw_genre_list):
@@ -28,19 +28,6 @@ def filter_games(singleplayer: bool, multiplayer: bool, raw_genre_list):
     Returns: list of game ids filtered by number of players and list of input genre
     """
     query = _gen_sql_query(raw_genre_list)
-    if query is not None:
-        res_games = eval(query)
-        # res_games = Game.query.filter(
-        #     Game.single_player == singleplayer, Game.multi_player == multiplayer,
-        #     (Game.genre.like('%action%')|Game.genre.like('%adventure%'))).limit(10)
-        res = [game.app_id for game in res_games]
-        # for game in res_games:
-        #     game_info = {
-        #         'id': game.app_id,
-        #         'name': game.name,
-        #         'genre': game.genre
-        #     }
-        #     res.append(game_info)
-        return res
-    else:
-        return None
+    res_games = eval(query)
+    res = [game.app_id for game in res_games]
+    return res
