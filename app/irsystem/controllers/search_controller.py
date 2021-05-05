eaaa64830a54ab782eb1137b4db92c119a807e4f
Ethan Hu <ethan.hqc@icloud.com>
2021-05-04 20:29:02 -0400

    front end support for games

--- app/irsystem/controllers/search_controller.py
+++ app/irsystem/controllers/search_controller.py
@@ -35,6 +35,8 @@ def search_action():
         'gameTags') else []
     movie = request.form.get('movieEnjoy') if request.form.get(
         'movieEnjoy') else None
+    game = request.form.get('gameEnjoy') if request.form.get(
+        'gameEnjoy') else None
     tags.extend(movie.split('_'))
     genres = json.loads(request.form.get('gameGenre')) if request.form.get(
         'gameGenre') else None  # TODO: add it to response
